6094f361a0a47664ed0270e2b9746c6d35aa8204
Dato Rogava <drogava@crocobet.local>
2025-05-02 18:22:02 +0400

    commit

---
+++ application/src/main/java/com/application/model/User.java
@@ -0,0 +1,17 @@
+package com.application.model;
+
+public class User {
+    private  int number;
+
+    public User(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
